f242022c8536ff1dca4b49b139e5f8d6f36cd81e
Parthiv Ghosh <g.parthiv119@gmail.com>
2026-03-14 16:36:32 +0000

    format summary numericals

--- src/components/portfolio/PortfolioSummary.jsx
+++ src/components/portfolio/PortfolioSummary.jsx
@@ -6,10 +6,20 @@ export default function PortfolioSummary() {
     <div>
       <h2>Portfolio Overview</h2>
 
-      <h3>Total Portfolio Value: {totVal.toLocaleString()}</h3>
+      <h3>
+        Total Portfolio Value:{" "}
+        {totVal.toLocaleString("en-IN", {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
+      </h3>
 
       <h3 style={{ color: totPnL >= 0 ? "green" : "red" }}>
-        Total Profit/Loss: {Math.abs(totPnL)}
+        Total Profit/Loss:{" "}
+        {Math.abs(totPnL).toLocaleString("en-IN", {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
       </h3>
     </div>
   )
